37014922072dc4ef961bcba0b13c7bc8088ab564
My Lam <my.lam@silkwires.com>
2020-07-01 13:52:55 +0700

    Add icon vector and run auto link: "react-native link react-native-vector-icons"

--- source/Views/home.js
+++ source/Views/home.js
@@ -4,27 +4,47 @@ import {
   Text,
   Dimensions,
   StyleSheet,
+  TouchableNativeFeedback,
 } from "react-native";
 import Video from 'react-native-video';
+import Icon from 'react-native-vector-icons/FontAwesome';
+// import { TouchableNativeFeedback } from 'react-native-gesture-handler';
 
 const { width } = Dimensions.get('window');
 export default class Home extends React.Component {
   constructor(p) {
     super(p);
     this.state = {
-      paused: false
+      paused: false,
+      overlay: false,
     };
   }
 
   render = () => {
-    const {paused} = this.state;
+    const {paused, overlay} = this.state;
     return (
       <View style={styles.container}>
         <View style={{width, height: width * .6, backgroundColor: 'black'}}>
           <Video
+            ref={ref => this.video = ref}
             source={{uri: "https://vjs.zencdn.net/v/oceans.mp4"}}   // Can be a URL or a local file.
             style={styles.backgroundVideo}
+            resizeMode='cover'
+            onLoad={this.load}
+            onProgress={this.progress}
           />
+          <View style={styles.overlay}>
+            {
+              !overlay ? <View style={{...styles.overlaySet, backgroundColor: '#0006'}}>
+                <Icon name='backward' style={styles.icon} onPress={this.backward}></Icon>
+                <Icon name={paused ? 'play' : 'pause'} style={styles.icon} ></Icon>
+                <Icon name='forward' style={styles.icon} onPress={this.forward}></Icon>
+              </View> : <View style={styles.overlaySet}>
+                <TouchableNativeFeedback onPress={this.youtubeSeekLeft}><View style={{ flex: 1 }}></View></TouchableNativeFeedback>
+                <TouchableNativeFeedback onPress={this.youtubeSeekRight}><View style={{ flex: 1 }}></View></TouchableNativeFeedback>
+              </View>
+            }
+          </View>
         </View>
       </View>
     )
@@ -39,8 +59,21 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   backgroundVideo: {
-    backgroundColor: "#fff",
     width: "100%",
     height: "100%"
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject
+  },
+  overlaySet: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  icon: {
+    color: 'white',
+    flex: 1,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    fontSize: 25
   }
 })
