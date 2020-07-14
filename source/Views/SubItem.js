ba0501c3875aa5321a5a76f282809ae75ab603ca
My Lam <my.lam@silkwires.com>
2020-07-14 14:53:33 +0700

    demo referent

--- source/Views/SubItem.js
+++ source/Views/SubItem.js
@@ -13,31 +13,45 @@ export default class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state={
-      colorViewNative: 'yellow',
       colorView: 'yellow'
     }
   }
 
   changeColorView() {
     this.setState({
-      colorViewNative: 'red'
+      colorView: 'red'
     })
   }
 
   render = () => {
-    const {colorView, colorViewNative} = this.state;
+    const {colorView} = this.state;
     return (
       <View style={styles.container}>
         <Text>Component Sub B</Text>
         <ScrollView>
-          <View style={styles.sessionChangeColor}>
-            <View style={[styles.viewBackground, {backgroundColor: colorViewNative}]}>
-              <Text>Backgrount change by native</Text>
-            </View>
-            <View style={[styles.viewBackground, {backgroundColor: colorView}]}>
-              <Text>Backgrount change by redux</Text>
-            </View>
-          </View>
+        <View style={[styles.viewBackground, {backgroundColor: colorView}]}>
+          <Text>Backgrount</Text>
+          <Text>Example
+To use, wrap your top level view with a SafeAreaView with a flex: 1 style applied to it. You may also want to use a background color that matches your application's design.</Text>
+          <Text>Props
+Inherits View Props.
+
+As padding is used to implement the behavior of the component, padding rules in styles applied to a SafeAreaView will be ignored and can cause different results depending on the platform. See #22211 for details.
+
+emulateUnlessSupported</Text>
+<Text>Props
+Inherits View Props.
+
+As padding is used to implement the behavior of the component, padding rules in styles applied to a SafeAreaView will be ignored and can cause different results depending on the platform. See #22211 for details.
+
+emulateUnlessSupported</Text>
+<Text>Props
+Inherits View Props.
+
+As padding is used to implement the behavior of the component, padding rules in styles applied to a SafeAreaView will be ignored and can cause different results depending on the platform. See #22211 for details.
+
+emulateUnlessSupported</Text>
+        </View>
         </ScrollView>
       </View>
     )
@@ -49,11 +63,8 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   viewBackground: {
-    width: 100,
-    height: 300,
-  },
-  sessionChangeColor: {
-    flexDirection: 'row',
-    justifyContent: 'space-between'
+    width: 300,
+    height: 800,
+
   }
 })
